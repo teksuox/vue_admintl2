b92e61e03a4464efc2549ec0d4d4243ec9593013
Teksuox <teksuox@gmail.com>
2019-08-28 16:33:52 -0400

    nada nuevo

--- src/components/Principal/Sidevar-Derecha/Sidevar.vue
+++ src/components/Principal/Sidevar-Derecha/Sidevar.vue
@@ -1,25 +1,180 @@
 <template>
-    <div>
-        <!-- sidevar de la derecha  -->
-        <aside class="control-sidebar control-sidebar-dark">
-            <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
-                <li class="active"><a href="#tab_1" data-toggle="tab" aria-expanded="true">Tab 1</a></li>
-                <li class=""><a href="#tab_2" data-toggle="tab" aria-expanded="false">Tab 2</a></li>
-            </ul>
-            <div class="tab-content">
-                <div class="tab-pane active" id="tab_1">
-                    <button @click="$store.state.skin = 'skin-red'" type="button" class="btn btn-block btn-danger">Rojo</button>
-                    <button @click="$store.state.skin = 'skin-blue'" type="button" class="btn btn-block btn-primary">Azul</button>
-                    <button @click="$store.state.skin = 'skin-black-light'" type="button" class="btn btn-block btn-default">Blanco</button>
+  <div>
+    <!-- sidevar de la derecha  -->
+    <aside class="control-sidebar control-sidebar-dark">
+      <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
+        <li class="active"><a href="#tab_1" data-toggle="tab" aria-expanded="true">Tab 1</a></li>
+        <li class=""><a href="#tab_2" data-toggle="tab" aria-expanded="false">Configuración Skin</a></li>
+      </ul>
+      <div class="tab-content">
+        <div class="tab-pane active" id="tab_1">
+
+        </div>
+        <div class="tab-pane" id="tab_2">
+          <ul class="list-unstyled clearfix">
+            <li style="float:left; width: 33.33333%; padding: 5px;">
+              <a @click="cambio_skin('skin-blue')" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">
+                <div>
+                  <span style="display:block; width: 20%; float: left; height: 7px; background: #367fa9"></span>
+                  <span class="bg-light-blue" style="display:block; width: 80%; float: left; height: 7px;"></span>
                 </div>
-                <div class="tab-pane" id="tab_2">
-                    
+                <div>	
+                  <span style="display:block; width: 20%; float: left; height: 20px; background: #222d32"></span>
+                  <span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span>
                 </div>
-            </div>
-        </aside>
-        <div class="control-sidebar-bg"></div>
-        <!-- Fin sidevar de la derecha  -->
-    </div>
+              </a>
+              <p class="text-center no-margin">Blue</p>
+            </li>
+            <li style="float:left; width: 33.33333%; padding: 5px;">
+              <a @click="cambio_skin('skin-black')" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">
+                <div style="box-shadow: 0 0 2px rgba(0,0,0,0.1)" class="clearfix">
+                  <span style="display:block; width: 20%; float: left; height: 7px; background: #fefefe"></span>
+                  <span style="display:block; width: 80%; float: left; height: 7px; background: #fefefe"></span>
+                </div>
+                <div>
+                  <span style="display:block; width: 20%; float: left; height: 20px; background: #222"></span>
+                  <span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span>
+                </div>
+              </a>
+              <p class="text-center no-margin">Black</p>
+            </li>
+            <li style="float:left; width: 33.33333%; padding: 5px;">
+              <a @click="cambio_skin('skin-purple')" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">
+                <div>
+                  <span style="display:block; width: 20%; float: left; height: 7px;" class="bg-purple-active"></span>
+                  <span class="bg-purple" style="display:block; width: 80%; float: left; height: 7px;"></span>
+                </div>
+                <div>
+                  <span style="display:block; width: 20%; float: left; height: 20px; background: #222d32"></span>
+                  <span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span>
+                </div>
+              </a>
+              <p class="text-center no-margin">Purple</p>
+            </li>
+            <li style="float:left; width: 33.33333%; padding: 5px;">
+              <a @click="cambio_skin('skin-green')" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">
+                <div>
+                  <span style="display:block; width: 20%; float: left; height: 7px;" class="bg-green-active"></span>
+                  <span class="bg-green" style="display:block; width: 80%; float: left; height: 7px;"></span>
+                </div>
+              <div>
+                  <span style="display:block; width: 20%; float: left; height: 20px; background: #222d32"></span>
+                  <span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span>
+                </div>
+              </a>
+              <p class="text-center no-margin">Green</p>
+            </li>
+            <li style="float:left; width: 33.33333%; padding: 5px;">
+              <a @click="cambio_skin('skin-red')" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">
+                <div>
+                  <span style="display:block; width: 20%; float: left; height: 7px;" class="bg-red-active"></span>
+                  <span class="bg-red" style="display:block; width: 80%; float: left; height: 7px;"></span>
+                </div>
+              <div>
+                  <span style="display:block; width: 20%; float: left; height: 20px; background: #222d32"></span>
+                  <span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span>
+                </div>
+              </a>
+              <p class="text-center no-margin">Red</p>
+            </li>
+            <li style="float:left; width: 33.33333%; padding: 5px;">
+              <a @click="cambio_skin('skin-yellow')" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">
+                <div>
+                  <span style="display:block; width: 20%; float: left; height: 7px;" class="bg-yellow-active"></span>
+                  <span class="bg-yellow" style="display:block; width: 80%; float: left; height: 7px;"></span>
+                </div>
+                <div>
+                  <span style="display:block; width: 20%; float: left; height: 20px; background: #222d32"></span>
+                  <span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span>
+                </div>
+              </a>
+              <p class="text-center no-margin">Yellow</p>
+            </li>
+            <li style="float:left; width: 33.33333%; padding: 5px;">
+              <a @click="cambio_skin('skin-blue-light')" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">
+                <div>
+                  <span style="display:block; width: 20%; float: left; height: 7px; background: #367fa9"></span>
+                  <span class="bg-light-blue" style="display:block; width: 80%; float: left; height: 7px;"></span>
+                </div>
+                <div>
+                  <span style="display:block; width: 20%; float: left; height: 20px; background: #f9fafc"></span>
+                  <span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span>
+                </div>
+              </a>
+              <p class="text-center no-margin" style="font-size: 12px">Blue Light</p>
+            </li>
+            <li style="float:left; width: 33.33333%; padding: 5px;">
+              <a @click="cambio_skin('skin-black-light')" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">
+                <div style="box-shadow: 0 0 2px rgba(0,0,0,0.1)" class="clearfix">
+                  <span style="display:block; width: 20%; float: left; height: 7px; background: #fefefe"></span>
+                  <span style="display:block; width: 80%; float: left; height: 7px; background: #fefefe"></span>
+                </div>
+                <div>
+                  <span style="display:block; width: 20%; float: left; height: 20px; background: #f9fafc"></span>
+                  <span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span>
+                </div>
+              </a>
+              <p class="text-center no-margin" style="font-size: 12px">Black Light</p>
+            </li>
+            <li style="float:left; width: 33.33333%; padding: 5px;">
+              <a @click="cambio_skin('skin-purple-light')" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">
+                <div>
+                  <span style="display:block; width: 20%; float: left; height: 7px;" class="bg-purple-active"></span>
+                  <span class="bg-purple" style="display:block; width: 80%; float: left; height: 7px;"></span>
+                </div>
+                <div>
+                  <span style="display:block; width: 20%; float: left; height: 20px; background: #f9fafc"></span>
+                  <span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span>
+                </div>
+              </a>
+              <p class="text-center no-margin" style="font-size: 12px">Purple Light</p>
+            </li>
+            <li style="float:left; width: 33.33333%; padding: 5px;">
+              <a @click="cambio_skin('skin-green-light')" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">
+                <div>
+                  <span style="display:block; width: 20%; float: left; height: 7px;" class="bg-green-active"></span>
+                  <span class="bg-green" style="display:block; width: 80%; float: left; height: 7px;"></span>
+                </div>
+                <div>
+                  <span style="display:block; width: 20%; float: left; height: 20px; background: #f9fafc"></span>
+                  <span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span>
+                </div>
+              </a>
+              <p class="text-center no-margin" style="font-size: 12px">Green Light</p>
+            </li>
+            <li style="float:left; width: 33.33333%; padding: 5px;">
+              <a @click="cambio_skin('skin-red-light')" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">
+                <div>
+                  <span style="display:block; width: 20%; float: left; height: 7px;" class="bg-red-active"></span>
+                  <span class="bg-red" style="display:block; width: 80%; float: left; height: 7px;"></span>
+                </div>
+                <div>
+                  <span style="display:block; width: 20%; float: left; height: 20px; background: #f9fafc"></span>
+                  <span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span>
+                </div>
+              </a>
+              <p class="text-center no-margin" style="font-size: 12px">Red Light</p>
+            </li>
+            <li style="float:left; width: 33.33333%; padding: 5px;">
+              <a @click="cambio_skin('skin-yellow-light')" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">
+                <div>
+                  <span style="display:block; width: 20%; float: left; height: 7px;" class="bg-yellow-active"></span>
+                  <span class="bg-yellow" style="display:block; width: 80%; float: left; height: 7px;"></span>
+                </div>
+                <div>
+                  <span style="display:block; width: 20%; float: left; height: 20px; background: #f9fafc"></span>
+                  <span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span>
+                </div>
+              </a>
+              <p class="text-center no-margin" style="font-size: 12px">Yellow Light</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </aside>
+    <div class="control-sidebar-bg"></div>
+    <!-- Fin sidevar de la derecha  -->
+  </div>
 </template>
 
 <script>
@@ -31,7 +186,9 @@ export default {
     }
   },
   methods:{
-    
+    cambio_skin(color){
+      this.$store.state.skin = color
+    }
   },
   mounted(){
       
--- src/store.js
+++ src/store.js
@@ -8,7 +8,7 @@ export default new Vuex.Store({
     skin : 'skin-red'
   },
   mutations: {
-
+    
   },
   actions: {
 
